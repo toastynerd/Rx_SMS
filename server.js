71d094e9a0631718a02314541565e9307931c832
Aliza Pilisuk <apilisuk@gmail.com>
2016-08-04 15:01:15 -0700

    setting up basic server and test to add travis

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const express = require('express');
+const app = express();
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server up on '+ (process.env.PORT || 3000));
+});
