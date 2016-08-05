a04da6f77e090c8841bf103f51b7c6d0a2b0b0d2
Aliza Pilisuk <apilisuk@gmail.com>
2016-08-05 12:04:59 -0700

    wrapped in function and exported to be used later

--- controller/sendgrid.js
+++ controller/sendgrid.js
@@ -1,20 +1,23 @@
 'use strict';
-let helper = require('sendgrid').mail,
-  from_email = new helper.Email('radcliffe.tracey@gmail.com'),
-  to_email = new helper.Email('2069312759@pm.sprint.com'),
-  subject = 'hello world',
-  content = new helper.Content('text/plain', 'AHHHHHHH'),
-  mail = new helper.Mail(from_email, subject, to_email, content);
 
-let sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-var request = sg.emptyRequest({
-  method: 'POST',
-  path: '/v3/mail/send',
-  body: mail.toJSON()
-});
+module.exports = exports = function(phoneEmail, drugs){
+  let helper = require('sendgrid').mail,
+    from_email = new helper.Email(process.env.SENDGRID_USERNAME),
+    to_email = new helper.Email(phoneEmail),
+    subject = 'hello world',
+    content = new helper.Content('text/plain', drugs),
+    mail = new helper.Mail(from_email, subject, to_email, content);
 
-sg.API(request, function(error, response) {
-  console.log(response.statusCode);
-  console.log(response.body);
-  console.log(response.headers);
-});
+  let sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+  var request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: mail.toJSON()
+  });
+
+  sg.API(request, function(error, response) {
+    console.log(response.statusCode);
+    console.log(response.body);
+    console.log(response.headers);
+  });
+};
