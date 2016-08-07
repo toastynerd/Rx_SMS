3506fd13c052fc8cebed0e6cbea8c76578b407f3
Aliza Pilisuk <apilisuk@gmail.com>
2016-08-06 20:07:25 -0700

    created a name to number function to get correct id for pulling data

---
+++ controller/nametonumber.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const request = require('request');
+
+module.exports = exports = function(name){
+  request('https://rxnav.nlm.nih.gov/REST/rxcui.json?name=' + name, function (error, response) {
+    if (!error && response.statusCode == 200) {
+      let bodyObj = JSON.parse(response.body);
+      let rxnormId = bodyObj.idGroup.rxnormId.toString();
+      console.log(rxnormId);
+    }
+  });
+};
