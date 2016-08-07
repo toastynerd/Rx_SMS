896334cea5596d1f8bc00bc9fb44a4ef89068af1
Aliza Pilisuk <apilisuk@gmail.com>
2016-08-07 11:33:57 -0700

    added another note and debugger statement

--- routes/route.js
+++ routes/route.js
@@ -7,6 +7,8 @@ let drugRouter = Router();
 
 drugRouter.post('/:drugname', (req, res, next) => {
   let interactions = rxnormIdHandler(req.params.drugname);
+  //interactions is undefined here because of scope issues at the end of the interactionhandler file.
+  debugger;
   console.log(interactions);
   let newDrug = new DrugSchema({'drug': req.params.drugname, 'interactions': interactions});
   newDrug.save((err, drugData) => {
