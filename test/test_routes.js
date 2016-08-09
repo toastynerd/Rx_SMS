6d91070fb68b9b6d62df897e478435235688b1a5
Tracey <radcliffe.tracey@gmail.com>
2016-08-09 14:54:32 -0700

    two working tests for user

--- test/test_routes.js
+++ test/test_routes.js
@@ -12,11 +12,45 @@ process.env.DB_SERVER = TEST_DB_SERVER;
 
 let app = require('./test_server');
 
-let server;
+let server, userID;
 
 describe('testing different routes for our server ', () => {
-  it('blah', (done) => {
-    expect('blah').to.eql('blah');
-    done();
+  before((done) =>{
+    server = app.listen(4000, ()=>{
+      console.log('Server on 4000');
+      done();
+    });
+  });
+  after((done) =>{
+    mongoose.connection.db.dropDatabase(()=>{
+      mongoose.disconnect(() => {
+        server.close();
+        done();
+      });
+    });
+  });
+  it('should create a new user', (done) => {
+    request('localhost:4000')
+      .post('/api/user/newUser')
+      .send({name: 'Tracey', phoneNumber:'123456078'})
+      .end((err, res)=>{
+        userID = res.body._id;
+        console.log(userID);
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('name');
+        done();
+      });
+  });
+  it('should GET a new user', (done) => {
+    request('localhost:4000')
+      .get('/api/user/' + userID)
+      .end((err, res)=>{
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('name');
+        expect(res.body.name).to.eql('Tracey');
+        done();
+      });
   });
 });
--- test/test_server.js
+++ test/test_server.js
@@ -1,10 +1,12 @@
+'use strict';
+
 const mongoose = require('mongoose');
 // const jwt_auth = require('../lib/jwt_auth');
 const serverError = require('debug')('rxsms:test_error');
 mongoose.connect('mongodb://localhost/routes_tests');
 let app = require('express')();
-const drugRoute = require('./routes/drug_route');
-const userRoute = require('./routes/user_route');
+const drugRoute = require('../routes/drug_route');
+const userRoute = require('../routes/user_route');
 
 app.use('/api/user', userRoute);
 app.use('/api/drug', drugRoute);
