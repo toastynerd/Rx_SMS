382c551605a384589868403f1ada64df92cb2265
Aliza Pilisuk <apilisuk@gmail.com>
2016-08-10 16:52:50 -0700

    fixed routing

--- routes/parse_route.js
+++ routes/parse_route.js
@@ -3,8 +3,39 @@ const Router = require('express').Router;
 const GridSchema = require('../models/sendgridschema');
 const jsonParser = require('body-parser').json();
 const sendGrid = require('../lib/sendgrid');
+const UserSchema = require('../models/userschema');
+const DrugSchema = require('../models/drugschema');
 const HandleError = require('../controller/errhandler');
 let parseRouter = Router();
+
+const unique = function(a) {
+  return Array.from(new Set(a));
+};
+
+const getInteractions = function(phoneEmail, drug) {
+  return new Promise((resolve, reject) => {
+    UserSchema.find({'phoneEmail': phoneEmail})
+      .then((user) => {
+        if(!user) return reject('User not found');
+        let userId = user._id;
+        let drugName = drug.trim();
+        DrugSchema.find({'userId': userId})
+          .then((drugs) => {
+            if (!drugs) return reject('Drugs not found');
+            var drugArray = [];
+            drugs.forEach(function(item) {
+              item.interactions.forEach(function(q) {
+                if (q.drugname === drugName){
+                  drugArray.push('Interaction between ' + item.drug + ' and ' + q.drugname + ': ' + q.interaction);
+                }
+              });
+            });
+            let uniqueArr = unique(drugArray);
+            resolve(uniqueArr.join(' ').toString());
+          });
+      });
+  });
+};
 
 parseRouter.post('/', jsonParser, function(req, res, next) {
   let preSplit = req.body.HtmlBody.toString().split('<PRE>');
@@ -17,7 +48,23 @@ parseRouter.post('/', jsonParser, function(req, res, next) {
     if (err) return next(err);
     res.json(grid);
   });
-  sendGrid(phoneEmail);
+  getInteractions(phoneEmail, content)
+    .then((data) => {
+      if(data.length === 0) data = 'no interactions found';
+      sendGrid(phoneEmail, data);
+    }, (err) => {
+      if(err) return HandleError (404, next, err);
+    });
+});
+
+parseRouter.get('/test/:phoneEmail/:drug', function(req, res, next) {
+  getInteractions(req.params.phoneEmail, req.params.drug)
+  .then((data) => {
+    if(data.length === 0) data = 'no interactions found';
+    sendGrid(req.params.phoneEmail, data);
+  }, (err) => {
+    if(err) return HandleError (404, next, err);
+  });
 });
 
 parseRouter.get('/', function(req, res, next) {
