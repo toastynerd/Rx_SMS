4032bdf27af045c74389e0f3d515a09d3314f7ff
Aliza Pilisuk <apilisuk@gmail.com>
2016-08-10 15:22:50 -0700

    working on testing sendgrid

--- routes/interaction_route.js
+++ routes/interaction_route.js
@@ -1,50 +1,73 @@
 'use strict';
 const Router = require('express').Router;
+const GridSchema = require('../models/sendgridschema');
+const UserSchema = require('../models/userschema');
 const DrugSchema = require('../models/drugschema');
+const jsonParser = require('body-parser').json();
+const sendGrid = require('../lib/sendgrid');
 const HandleError = require('../controller/errhandler');
-let interactionRouter = module.exports = exports = Router();
 const unique = function(a) {
   return Array.from(new Set(a));
 };
+let parseRouter = Router();
 
-interactionRouter.get('/interactions/:userId/:drugName', function(req, res, next) {
-  let drugName = req.params.drugName.split('%').join(' ');
-  DrugSchema.find({'userId': req.params.userId})
-  .then((drugs) => {
-    if (!drugs) return HandleError(404, next)(new Error('Drugs not found'));
-    var drugArray = [];
-    drugs.forEach(function(item) {
-      item.interactions.forEach(function(q) {
-        if (q.drugname === drugName){
-          drugArray.push('Interaction between ' + item.drug + ' and ' + q.drugname + ': ' + q.interaction);
-        }
+const getInteractions = function(phoneEmail, drug) {
+  return new Promise((resolve, reject) => {
+    UserSchema.find({'phoneEmail': phoneEmail})
+      .then((user) => {
+        if(!user) return reject('User not found');
+        let userId = user._id;
+        let drugName = drug.trim();
+        DrugSchema.find({'userId': userId})
+          .then((drugs) => {
+            if (!drugs) return reject('Drugs not found');
+            var drugArray = [];
+            drugs.forEach(function(item) {
+              item.interactions.forEach(function(q) {
+                if (q.drugname === drugName){
+                  drugArray.push('Interaction between ' + item.drug + ' and ' + q.drugname + ': ' + q.interaction);
+                }
+              });
+            });
+            let uniqueArr = unique(drugArray);
+            resolve(uniqueArr.join(' ').toString());
+          });
       });
-    });
-    if (drugArray.length > 0) {
-      let uniqueArr = unique(drugArray);
-      res.json(uniqueArr.join(' ').toString());
-      next();
-    }
-    return HandleError(404, next)(new Error('No interactions found'));
+  });
+};
+
+parseRouter.get('/test/:phoneEmail/:drug', function(req, res, next) {
+  getInteractions(req.params.phoneEmail, req.params.drug)
+  .then((data) => {
+    if(data.length === 0) data = 'no interactions found';
+    sendGrid(req.params.phoneEmail, data);
+  }, (err) => {
+    if(err) return HandleError (404, next, err);
   });
 });
 
-interactionRouter.get('/interactions/:userId', function(req, res, next) {
-  DrugSchema.find({'userId': req.params.userId})
-  .then((drugs) => {
-    if (!drugs) return HandleError(404, next)(new Error('Drugs not found'));
-    var drugArray = [];
-    drugs.forEach(function(item) {
-      drugArray.push('DRUGS INTERACTING WITH ' + item.drug.toUpperCase() + ':');
-      item.interactions.forEach(function(q) {
-        drugArray.push(q.drugname);
-      });
-    });
-    if (drugArray.length > 0) {
-      let uniqueArr = unique(drugArray);
-      res.json(uniqueArr);
-      next();
-    }
-    return HandleError(404, next)(new Error('No interactions found'));
+parseRouter.post('/', jsonParser, function(req, res, next) {
+  let preSplit = req.body.HtmlBody.toString().split('<PRE>');
+  let afterSplit = preSplit[1].split('</PRE>');
+  let content = afterSplit[0];
+  let phoneEmail = req.body.From;
+  console.log('email body: ', content);
+  let gridSchema = new GridSchema({'phoneNumber': phoneEmail, 'text': content});
+  gridSchema.save((err, grid) => {
+    if (err) return next(err);
+    res.json(grid);
   });
+  getInteractions(phoneEmail, content)
+    .then((data) => {
+      if(data.length === 0) data = 'no interactions found';
+      sendGrid(phoneEmail, data);
+    }, (err) => {
+      if(err) return HandleError (404, next, err);
+    });
 });
+
+parseRouter.get('/', function(req, res, next) {
+  GridSchema.find().then(res.json.bind(res), HandleError(500, next, 'Server error!'));
+});
+
+module.exports = exports = parseRouter;
