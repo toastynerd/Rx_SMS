2b9aea5ad85baa85f8274a4f0f352448fd4990b8
Aliza Pilisuk <apilisuk@gmail.com>
2016-08-09 20:12:01 -0700

    removed interactions that were displayed twice with unique function. Changed error for no interactions found. Changed server to 3000. Fixed interaction response to be more human readable.

--- routes/interaction_route.js
+++ routes/interaction_route.js
@@ -3,6 +3,9 @@ const Router = require('express').Router;
 const DrugSchema = require('../models/drugschema');
 const HandleError = require('../controller/errhandler');
 let interactionRouter = module.exports = exports = Router();
+const unique = function(a) {
+  return Array.from(new Set(a));
+};
 
 interactionRouter.get('/interactions/:userId/:drugName', function(req, res, next) {
   let drugName = req.params.drugName;
@@ -13,15 +16,15 @@ interactionRouter.get('/interactions/:userId/:drugName', function(req, res, next
     drugs.forEach(function(item) {
       item.interactions.forEach(function(q) {
         if (q.drugname === drugName){
-          drugArray.push(q.interaction);
-          console.log('q.drugname', drugArray);
+          drugArray.push('Interaction between ' + item.drug + ' and ' + q.drugname + ': ' + q.interaction);
         }
       });
     });
     if (drugArray.length > 0) {
-      res.json(drugArray.toString());
+      let uniqueArr = unique(drugArray);
+      res.json(uniqueArr.join(' ').toString());
       next();
     }
-    return HandleError(404, next)(new Error('Drugs not found'));
+    return HandleError(404, next)(new Error('No interactions found'));
   });
 });
