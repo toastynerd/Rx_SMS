4dcdb58c950cb76b25090e344d2eba902a0b3cbf
Tracey <radcliffe.tracey@gmail.com>
2016-08-10 17:39:51 -0700

    auth for users working

--- models/userschema.js
+++ models/userschema.js
@@ -2,6 +2,8 @@
 
 const mongoose = require('mongoose');
 const DrugSchema = require('./drugschema');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
   phoneNumber: {
@@ -17,7 +19,8 @@ const UserSchema = new mongoose.Schema({
   basic: {
     username: {
       type: String,
-      required: true
+      required: true,
+      unique: true
     },
     password: {
       type: String,
@@ -44,4 +47,24 @@ UserSchema.methods.removeDrug = function(drugId) {
   return DrugSchema.findOneAndUpdate({'_id': drugId}, {userId: null});
 };
 
-module.exports = mongoose.model('User', UserSchema);
+UserSchema.methods.createHash = function(password){
+  return new Promise((resolve, reject)=>{
+    bcrypt.hash(password, 10, (err, data) =>{
+      if (err) return reject(err);
+      this.basic.password = data;
+      resolve({token: jwt.sign({idd: this.basic.username}, process.env.APP_SECRET)});
+    });
+  });
+};
+
+UserSchema.methods.comparePass = function(password){
+  return new Promise((resolve, reject) =>{
+    bcrypt.compare(password, this.basic.password, (err, data) => {
+      if (err) return reject(err);
+      if (data === false) return reject(new Error('No matching password'));
+      resolve({token: jwt.sign({idd: this.basic.username}, process.env.APP_SECRET)});
+    });
+  });
+};
+
+module.exports = exports = mongoose.model('User', UserSchema);
